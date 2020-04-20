2bfd71bf5e2df4a31ac4fd83bea2ae98ff3498da
Gabriel Nadon <gnadon@bisscomm.com>
2020-04-19 22:49:39 -0400

    mobile menu

--- js/app.js
+++ js/app.js
@@ -30,43 +30,68 @@ $owl.owlCarousel({
     loop: true,
     item: 3,
     onDragged: callback,
-    responsive:{
-        0:{
-            items:1,
+    responsive: {
+        0: {
+            items: 1,
             onDragged: callback,
         },
 
 
-        1300:{
-            items:3,
+        1300: {
+            items: 3,
 
         }
     }
 });
-if(window.innerWidth <= 1300){
+if (window.innerWidth <= 1300) {
     $('.owl-item.active').eq(0).addClass('center');
-}
-else {
+} else {
     $('.owl-item.active').eq(1).addClass('center');
 }
 
-$('#arrow-right').click(function() {
+$('#arrow-right').click(function () {
 
     $owl.trigger('next.owl.carousel');
     callback();
 });
 
-$('#arrow-left').click(function() {
+$('#arrow-left').click(function () {
     $owl.trigger('prev.owl.carousel', [300]);
     callback();
 });
 
 function callback(event) {
 
-    if(window.innerWidth <= 1300){
+    if (window.innerWidth <= 1300) {
         $('.owl-item.active').eq(0).addClass('center');
-    }
-    else {
+    } else {
         $('.owl-item.active').eq(1).addClass('center');
     }
 }
+
+
+let clicked = false;
+$('.menu-mobile .has-submenu').click(function () {
+    if (clicked == false) {
+        $('.menu-mobile .submenu').removeClass('d-none');
+        clicked = true;
+    } else {
+        $('.menu-mobile .submenu').addClass('d-none');
+        clicked = false;
+    }
+});
+
+let open = false;
+$('#button-mobile-menu').on('click', function () {
+    $('.navbar-toggle').toggleClass('opened');
+    if(open == false){
+        $('.menu-mobile').removeClass('d-none');
+        $('html').addClass('disable-overflow');
+        open = true;
+    }
+    else {
+        $('.menu-mobile').addClass('d-none');
+        $('html').removeClass('disable-overflow');
+        open = false;
+    }
+});
